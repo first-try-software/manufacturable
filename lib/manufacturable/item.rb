9c6bbba99ce6f99551fde62fac733f0a30333cb8
Alan Ridlehoover <alan@ridlehoover.com>
2020-06-14 16:39:25 -0700

    Allows Manufacturable::Item to receive explicit type when registering with factory
    Co-authored-by: Fito von Zastrow <adolfovon@gmail.com>

--- lib/manufacturable/item.rb
+++ lib/manufacturable/item.rb
@@ -2,17 +2,17 @@ require 'manufacturable/registrar'
 
 module Manufacturable
   module Item
-    def corresponds_to(key)
-      key = key == self.superclass ? Registrar::ALL_KEY : key
-      Registrar.register(self.superclass, key, self)
+    def corresponds_to(key, type = self.superclass)
+      key = key == type ? Registrar::ALL_KEY : key
+      Registrar.register(type, key, self)
     end
 
-    def corresponds_to_all
-      corresponds_to(Registrar::ALL_KEY)
+    def corresponds_to_all(type = self.superclass)
+      corresponds_to(Registrar::ALL_KEY, type)
     end
 
-    def default_manufacturable
-      corresponds_to(Registrar::DEFAULT_KEY)
+    def default_manufacturable(type = self.superclass)
+      corresponds_to(Registrar::DEFAULT_KEY, type)
     end
   end
 end
--- spec/manufacturable/item_spec.rb
+++ spec/manufacturable/item_spec.rb
@@ -9,44 +9,80 @@ RSpec.describe Manufacturable::Item do
   end
 
   describe '.corresponds_to' do
-    subject(:corresponds_to) { item.corresponds_to(key) }
+    subject(:corresponds_to) { item.corresponds_to(*args) }
+
+    let(:args) { [key] }
 
     before { corresponds_to }
 
-    context 'when the key does NOT match the item type' do
-      let(:key) { :key }
+    context 'when the type is NOT provided' do
+      context 'when the key does NOT match the item type' do
+        let(:key) { :key }
 
-      it 'registers itself with the key' do
-        expect(Manufacturable::Registrar).to have_received(:register).with(type, key, item)
+        it 'registers itself with the key' do
+          expect(Manufacturable::Registrar).to have_received(:register).with(type, key, item)
+        end
+      end
+
+      context 'when the key matches the item type' do
+        let(:key) { type }
+
+        it 'registers itself for all items of that type' do
+          expect(Manufacturable::Registrar).to have_received(:register).with(type, Manufacturable::Registrar::ALL_KEY, item)
+        end
       end
     end
 
-    context 'when the key matches the item type' do
-      let(:key) { type }
+    context 'when the type is provided' do
+      let(:args) { [key, explicit_type] }
+      let(:key) { :key }
+      let(:explicit_type) { :explicit_type }
 
-      it 'registers itself for all items of that type' do
-        expect(Manufacturable::Registrar).to have_received(:register).with(type, Manufacturable::Registrar::ALL_KEY, item)
+      it 'registers itself with the key and type' do
+        expect(Manufacturable::Registrar).to have_received(:register).with(explicit_type, key, item)
       end
     end
   end
 
   describe '.corresponds_to_all' do
-    subject(:corresponds_to_all) { item.corresponds_to_all }
+    subject(:corresponds_to_all) { item.corresponds_to_all(*args) }
+
+    let(:args) { [] }
 
     before { corresponds_to_all }
 
     it 'registers itself for all items of that type' do
       expect(Manufacturable::Registrar).to have_received(:register).with(type, Manufacturable::Registrar::ALL_KEY, item)
     end
+
+    context 'when the type is provided' do
+      let(:args) { [explicit_type] }
+      let(:explicit_type) { :explicit_type }
+
+      it 'registers itself for all items of the specified type' do
+        expect(Manufacturable::Registrar).to have_received(:register).with(explicit_type, Manufacturable::Registrar::ALL_KEY, item)
+      end
+    end
   end
 
   describe '.default_manufacturable' do
-    subject(:default_manufacturable) { item.default_manufacturable }
+    subject(:default_manufacturable) { item.default_manufacturable(*args) }
+
+    let(:args) { [] }
 
     before { default_manufacturable }
 
     it 'registers itself as the default item for that type' do
       expect(Manufacturable::Registrar).to have_received(:register).with(type, Manufacturable::Registrar::DEFAULT_KEY, item)
+    end
+
+    context 'when the type is provided' do
+      let(:args) { [explicit_type] }
+      let(:explicit_type) { :explicit_type }
+
+      it 'registers itself as the default item for the specified type' do
+        expect(Manufacturable::Registrar).to have_received(:register).with(explicit_type, Manufacturable::Registrar::DEFAULT_KEY, item)
+      end
     end
   end
 end
